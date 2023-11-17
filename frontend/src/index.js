017dd451f5f5d54ca0e755ae218a8a52c00a2e0b
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-11-17 16:51:47 +0000

    slice api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,8 @@ import {
 import "./assets/styles/bootstrap.custom.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider } from "react-redux";
+import store from "./store";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
@@ -26,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
